2bf435f81aaaadedf049c6a76034d3534287e9ea
funny122079 <funny122079@gmail.com>
2024-02-20 18:22:47 -0800

    get balance function added

--- constants.js
+++ constants.js
@@ -3,6 +3,7 @@ module.exports = {
     tokenContractAddress: {
         'Ethereum': {
             'WETH' : '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',  
+            'USDT' : '0xdAC17F958D2ee523a2206206994597C13D831ec7' 
         },
         'BSC': {
             'WBNB' : '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
